46446f37ba22565f00d14c40cffce4489e6d1568
Malisha Rosa <malishalakshandev@gmail.com>
2025-08-08 11:46:51 +0530

    create src/api/payment.ts -->>  api routes file for payment handling works  | and create src/application/payment.ts --->> aplication file for handling payment functions for payment  api routes and etc

---
+++ src/api/payment.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { createCheckoutSession, retrieveSessionStatus } from "../application/payment";
+
+export const paymentsRouter = express.Router();
+
+paymentsRouter.route("/create-checkout-session").post(createCheckoutSession);
+paymentsRouter.route("/session-status").get(retrieveSessionStatus);
